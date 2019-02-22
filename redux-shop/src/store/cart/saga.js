e1c74cce8ec6e351ff37efe767fdda5abc0525af
Rafał Kostrzewski <poczta.raf.kos@gmail.com>
2019-02-22 22:26:43 +0100

    Redux saga exercise implemented

--- redux-shop/src/store/cart/saga.js
+++ redux-shop/src/store/cart/saga.js
@@ -1,6 +1,28 @@
+import { take, race, call } from 'redux-saga/effects';
+import { delay } from 'redux-saga'
+import { ADD_PRODUCT_TO_CART, CHECKOUT } from './constants';
+
 export default function* cartSaga() {
-  // TODO: Implement
-  yield;
+  const productsAddedToCart = [];
+
+  while (productsAddedToCart.length < 3) {
+    const action = yield take(ADD_PRODUCT_TO_CART);
+
+    if (!productsAddedToCart.includes(action.productId)) {
+      productsAddedToCart.push(action.productId)
+    }
+  }
+
+  const { checkout } = yield race({
+    checkout: take(CHECKOUT),
+    timeout: call(delay, 30 * 1000)
+  })
+
+  if (checkout) {
+    return;
+  }
+
+  yield call(notifyProductsAddedToCartWithoutCheckout);
 }
 
 function notifyProductsAddedToCartWithoutCheckout() {
